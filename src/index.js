c4f1568c8fa38fe4f22a1846fe4df686070c66f5
a-florida-key <a.florida.key@gmail.com>
2023-12-11 20:39:22 -0500

    Updated JS to update time every second

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-let torontoDateElement = document.querySelector("#toronto-date");
-let torontoTimeElement = document.querySelector("#toronto-time");
-torontoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-torontoTimeElement.innerHTML = moment
-  .tz("America/Toronto")
-  .format("h:mm:ss [<small>]A[</small>]");
+setInterval(function () {
+  let torontoDateElement = document.querySelector("#toronto-date");
+  let torontoTimeElement = document.querySelector("#toronto-time");
+  torontoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+  torontoTimeElement.innerHTML = moment
+    .tz("America/Toronto")
+    .format("h:mm:ss [<small>]A[</small>]");
 
-let londonDateElement = document.querySelector("#london-date");
-let londonTimeElement = document.querySelector("#london-time");
-londonDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-londonTimeElement.innerHTML = moment
-  .tz("Europe/London")
-  .format("h:mm:ss [<small>]A[</small>]");
+  let londonDateElement = document.querySelector("#london-date");
+  let londonTimeElement = document.querySelector("#london-time");
+  londonDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+  londonTimeElement.innerHTML = moment
+    .tz("Europe/London")
+    .format("h:mm:ss [<small>]A[</small>]");
+}, 1000);
