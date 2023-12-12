118041ada270ca57c054d827b0c2255bd952ad9b
a-florida-key <a.florida.key@gmail.com>
2023-12-11 20:21:16 -0500

    updated am/pm JS

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ let torontoTimeElement = document.querySelector("#toronto-time");
 torontoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
 torontoTimeElement.innerHTML = moment
   .tz("Asia/Tokyo")
-  .format("h:mm [<small>A</small>]");
+  .format("h:mm [<small>]A[</small>]");
