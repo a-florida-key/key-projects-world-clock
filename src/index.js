3291bd53051f291f2150aad56af552b15efe0c3f
a-florida-key <a.florida.key@gmail.com>
2023-12-11 20:20:18 -0500

    Add small tags to JS AM/PM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 let torontoDateElement = document.querySelector("#toronto-date");
 let torontoTimeElement = document.querySelector("#toronto-time");
 torontoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
-torontoTimeElement.innerHTML = moment.tz("Asia/Tokyo").format("h:mm A");
+torontoTimeElement.innerHTML = moment
+  .tz("Asia/Tokyo")
+  .format("h:mm [<small>A</small>]");
