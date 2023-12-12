6651ff630a546947198852544b96116f0d76ffcd
a-florida-key <a.florida.key@gmail.com>
2023-12-11 19:53:25 -0500

    Updated JS for Tokyo date and time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 let tokyoElement = document.querySelector("#tokyo");
 let tokyoDateElement = document.querySelector("#tokyo.date");
 let tokyoTimeElement = document.querySelector("#tokyo.time");
+tokyoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+tokyoTimeElement.innerHTML = moment().format("h:m A");
