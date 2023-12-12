04896bfed3e16c105e76124a29be1b9d6fe6c354
a-florida-key <a.florida.key@gmail.com>
2023-12-11 20:24:02 -0500

    Corrected timezone for Toronto; added JS for London

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ let torontoDateElement = document.querySelector("#toronto-date");
 let torontoTimeElement = document.querySelector("#toronto-time");
 torontoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
 torontoTimeElement.innerHTML = moment
-  .tz("Asia/Tokyo")
+  .tz("America/Toronto")
+  .format("h:mm [<small>]A[</small>]");
+
+let londonDateElement = document.querySelector("#london-date");
+let londonTimeElement = document.querySelector("#ondon-time");
+londonDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+londonTimeElement.innerHTML = moment
+  .tz("Europe/London")
   .format("h:mm [<small>]A[</small>]");
