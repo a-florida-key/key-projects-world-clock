aa56703e1ce2b86ed0834034dfa878c5cee4a004
a-florida-key <a.florida.key@gmail.com>
2023-12-11 21:08:53 -0500

    Updated HTML and JS to replace City and current time when city is selected

--- src/index.js
+++ src/index.js
@@ -12,5 +12,41 @@ function updateCityTimes() {
   londonTimeElement.innerHTML = moment
     .tz("Europe/London")
     .format("h:mm:ss [<small>]A[</small>]");
+
+  let chicagoDateElement = document.querySelector("#chicago-date");
+  let chicagoTimeElement = document.querySelector("#chicago-time");
+  chicagoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+  chicagoTimeElement.innerHTML = moment
+    .tz("America/chicago")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  let sydneyDateElement = document.querySelector("#sydney-date");
+  let sydneyTimeElement = document.querySelector("#sydney-time");
+  sydneyDateElement.innerHTML = moment().format("MMMM Do, YYYY");
+  sydneyTimeElement.innerHTML = moment
+    .tz("Australia/Sydney")
+    .format("h:mm:ss [<small>]A[</small>]");
 }
 setInterval(updateCityTimes, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+        <div>
+          <h2>${cityTimeZone}</h2>
+          <div class="date" id="toronto-date">${cityTime.format(
+            "MMMM Do YYYY"
+          )}</div>
+        </div>
+        <div>
+          <div class="time" id="toronto-time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>
+      </div>`;
+}
+
+let citiesSelect = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
