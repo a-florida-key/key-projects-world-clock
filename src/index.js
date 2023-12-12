8d24c35df9b77b4556de105345f7fbc3a3bf304b
a-florida-key <a.florida.key@gmail.com>
2023-12-11 20:25:20 -0500

    Updated time JS

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ let torontoTimeElement = document.querySelector("#toronto-time");
 torontoDateElement.innerHTML = moment().format("MMMM Do, YYYY");
 torontoTimeElement.innerHTML = moment
   .tz("America/Toronto")
-  .format("h:mm [<small>]A[</small>]");
+  .format("h:mm:ss [<small>]A[</small>]");
 
 let londonDateElement = document.querySelector("#london-date");
 let londonTimeElement = document.querySelector("#ondon-time");
 londonDateElement.innerHTML = moment().format("MMMM Do, YYYY");
 londonTimeElement.innerHTML = moment
   .tz("Europe/London")
-  .format("h:mm [<small>]A[</small>]");
+  .format("h:mm:ss [<small>]A[</small>]");
